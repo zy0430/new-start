7300f5cd82f8daa3a663350c78177a8e3042eead
LAPTOP-UDSNSP26\章鱼哥 <648883413@qq.com>
2020-12-08 23:45:21 +0800

    shiro 刚把数据库创建好

---
+++ spring-boot-shiro/src/test/java/com/example/SpringBootShiroApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.example.model.SysPermission;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+        SysPermission sysPermission = new SysPermission();
+    }
+}
